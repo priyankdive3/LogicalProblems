fa22d218c96e505611152bf8e0503631a3dc1c73
priyanka <priyanka.divekar1100@gmail.com>
2022-09-01 13:50:56 +0530

    [Priyanka]Monthly Payment

---
+++ LogicalPrograms/MonthlyPayment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    internal class MonthlyPayment
+    {
+        public static void Payment()
+        {
+            Console.WriteLine("Enter Pricipal");
+            double p = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Year");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Rate");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            double n = 12 * y;
+            double r1 = r / (12 * 100);
+
+            double payment = p * r1 / 1 - Math.Pow((1 + r), (-n));
+            Console.WriteLine("Payment : " + payment);
+
+        }
+    }
+}
